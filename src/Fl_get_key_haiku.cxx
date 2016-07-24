4c78389dd16ccaefdb75b09fb50b84c9ae6d2422
François Revol <revol@free.fr>
2016-07-24 17:22:12 +0200

    Haiku: stub out get_key support

---
+++ src/Fl_get_key_haiku.cxx
@@ -0,0 +1,39 @@
+//
+// "$Id: Fl_get_key_haiku.cxx 10747 2015-06-10 10:53:25Z manolo $"
+//
+// MacOS keyboard state routines for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2015 by Bill Spitzak and others.
+//
+// This library is free software. Distribution and use rights are outlined in
+// the file "COPYING" which should have been included with this file.  If this
+// file is missing or damaged, see the license at:
+//
+//     http://www.fltk.org/COPYING.php
+//
+// Please report all bugs and problems on the following page:
+//
+//     http://www.fltk.org/str.php
+//
+
+// Return the current state of a key.  Keys are named by fltk symbols,
+// which are actually X keysyms.  So this has to translate to Haiku
+// symbols.
+
+#include <FL/Fl.H>
+#include <FL/x.H>
+
+#warning // TODO
+int Fl::event_key(int k) {
+#warning // TODO
+  return 0;
+}
+
+int Fl::get_key(int k) {
+#warning // TODO
+  return 0;
+}
+
+//
+// End of "$Id: Fl_get_key_haiku.cxx 10747 2015-06-10 10:53:25Z manolo $".
+//
